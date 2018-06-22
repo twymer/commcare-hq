1ab0ba2f243496e649b3852aba1231f043ef38a5
Simon Kelly <skelly@dimagi.com>
2018-06-22 13:45:32 +0200

    require recent timestamp with hmac auth

--- corehq/util/hmac_request.py
+++ corehq/util/hmac_request.py
@@ -3,13 +3,18 @@ from __future__ import unicode_literals
 import base64
 import hashlib
 import hmac
+from datetime import datetime, timedelta
 from functools import wraps
+
+import iso8601
 import six
 
 from django.conf import settings
 from django.http import HttpResponse
 
 from corehq.util.soft_assert.api import soft_assert
+
+ACCEPTABLE_DELAY_SECONDS = 30
 
 _soft_assert = soft_assert(notify_admins=True)
 
@@ -24,6 +29,16 @@ def get_hmac_digest(shared_key, data):
     return digest
 
 
+def timestamp_valid(timestamp_string):
+    try:
+        timestamp = iso8601.parse_date(timestamp_string)
+    except iso8601.ParseError:
+        return False
+
+    seconds_diff = (datetime.utcnow() - timestamp).total_seconds()
+    return 0 < seconds_diff < ACCEPTABLE_DELAY_SECONDS
+
+
 def validate_request_hmac(setting_name, ignore_if_debug=False):
     """
     Decorator to validate request sender using a shared secret
@@ -32,8 +47,10 @@ def validate_request_hmac(setting_name, ignore_if_debug=False):
 
     Example request:
 
-        digest = base64.b64encode(hmac.new(shared_secret, data, hashlib.sha256).digest())
-        requests.post(url, data=data, headers={'X-MAC-DIGEST': digest})
+        timestamp = datetime.utcnow().isoformat()
+        hmac_data = post_data + timestamp
+        digest = base64.b64encode(hmac.new(shared_secret, hmac_data, hashlib.sha256).digest())
+        requests.post(url, data=post_data, headers={'X-MAC-DIGEST': digest})
 
     :param setting_name: The name of the Django setting that holds the secret key
     :param ignore_if_debug: If set to True this is completely ignored if settings.DEBUG is True
@@ -51,7 +68,12 @@ def validate_request_hmac(setting_name, ignore_if_debug=False):
             if not expected_digest or not shared_key:
                 return HttpResponse(status=401)
 
-            digest = get_hmac_digest(shared_key, request.body)
+            timestamp = request.META.get('HTTP_X_MAC_TS', None)
+            if not timestamp_valid(timestamp):
+                return HttpResponse(status=400)
+
+            data = request.body + timestamp
+            digest = get_hmac_digest(shared_key, data)
 
             if expected_digest != digest:
                 return HttpResponse(status=401)
