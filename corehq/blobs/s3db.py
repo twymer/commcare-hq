b549a3bc6f55829a954091f765ff323afa37300b
Daniel Miller <dmiller@dimagi.com>
2018-06-25 12:22:30 -0400

    Adapt s3db to new blob metadata API

--- corehq/blobs/s3db.py
+++ corehq/blobs/s3db.py
@@ -5,10 +5,9 @@ import weakref
 from contextlib import contextmanager
 from io import UnsupportedOperation
 
-from corehq.blobs import BlobInfo, DEFAULT_BUCKET
 from corehq.blobs.exceptions import BadName, NotFound
 from corehq.blobs.interface import AbstractBlobDB, SAFENAME
-from corehq.blobs.util import ClosingContextProxy, set_blob_expire_object
+from corehq.blobs.util import ClosingContextProxy
 from corehq.util.datadog.gauges import datadog_counter, datadog_bucket_timer
 from dimagi.utils.logging import notify_exception
 
@@ -26,6 +25,7 @@ DEFAULT_S3_BUCKET = "blobdb"
 class S3BlobDB(AbstractBlobDB):
 
     def __init__(self, config):
+        super(S3BlobDB, self).__init__()
         kwargs = {}
         if "config" in config:
             kwargs["config"] = Config(**config["config"])
@@ -41,15 +41,14 @@ class S3BlobDB(AbstractBlobDB):
         # https://github.com/boto/boto3/issues/259
         self.db.meta.client.meta.events.unregister('before-sign.s3', fix_s3_host)
 
-    def report_timing(self, action, identifier, bucket):
+    def report_timing(self, action, path):
         def record_long_request(duration):
             if duration > 100:
                 notify_exception(None, "S3BlobDB request took a long time.", details={
                     'duration': duration,
                     's3_bucket_name': self.s3_bucket_name,
                     'action': action,
-                    'identifier': identifier,
-                    'blobdb_bucket': bucket,
+                    'path': path,
                 })
 
         return datadog_bucket_timer('commcare.blobs.requests.timing', tags=[
@@ -57,99 +56,74 @@ class S3BlobDB(AbstractBlobDB):
             's3_bucket_name:{}'.format(self.s3_bucket_name)
         ], timing_buckets=(.03, .1, .3, 1, 3, 10, 30, 100), callback=record_long_request)
 
-    def put(self, content, identifier, bucket=DEFAULT_BUCKET, timeout=None):
-        path = self.get_path(identifier, bucket)
+    def put(self, content, **blob_meta_args):
+        meta = self.metadb.new(**blob_meta_args)
+        check_safe_path(meta.path)
         s3_bucket = self._s3_bucket(create=True)
         if isinstance(content, BlobStream) and content.blob_db is self:
+            obj = s3_bucket.Object(content.blob_path)
+            meta.content_length = obj.content_length
+            self.metadb.put(meta)
             source = {"Bucket": self.s3_bucket_name, "Key": content.blob_path}
-            s3_bucket.copy(source, path)
-            obj = s3_bucket.Object(path)
-            # unfortunately cannot get content-md5 here
-            return BlobInfo(identifier, obj.content_length, None)
-        content.seek(0)
-        content_md5 = get_content_md5(content)
-        content_length = get_file_size(content)
-        with self.report_timing('put', identifier, bucket):
-            s3_bucket.upload_fileobj(content, path)
-        if timeout is not None:
-            set_blob_expire_object(bucket, identifier, content_length, timeout)
-        datadog_counter('commcare.blobs.added.count')
-        datadog_counter('commcare.blobs.added.bytes', value=content_length)
-        return BlobInfo(identifier, content_length, "md5-" + content_md5)
+            with self.report_timing('put-via-copy', meta.path):
+                s3_bucket.copy(source, meta.path)
+        else:
+            content.seek(0)
+            meta.content_length = get_file_size(content)
+            self.metadb.put(meta)
+            with self.report_timing('put', meta.path):
+                s3_bucket.upload_fileobj(content, meta.path)
+        return meta
 
-    def get(self, identifier, bucket=DEFAULT_BUCKET):
-        path = self.get_path(identifier, bucket)
-        with maybe_not_found(throw=NotFound(identifier, bucket)), \
-                self.report_timing('get', identifier, bucket):
+    def get(self, path):
+        check_safe_path(path)
+        with maybe_not_found(throw=NotFound(path)), self.report_timing('get', path):
             resp = self._s3_bucket().Object(path).get()
         return BlobStream(resp["Body"], self, path)
 
-    def size(self, identifier, bucket=DEFAULT_BUCKET):
-        path = self.get_path(identifier, bucket)
-        with maybe_not_found(throw=NotFound(identifier, bucket)), \
-                self.report_timing('size', identifier, bucket):
+    def size(self, path):
+        check_safe_path(path)
+        with maybe_not_found(throw=NotFound(path)), self.report_timing('size', path):
             return self._s3_bucket().Object(path).content_length
 
-    def exists(self, identifier, bucket=DEFAULT_BUCKET):
-        path = self.get_path(identifier, bucket)
+    def exists(self, path):
+        check_safe_path(path)
         try:
-            with maybe_not_found(throw=NotFound(identifier, bucket)), \
-                    self.report_timing('exists', identifier, bucket):
+            with maybe_not_found(throw=NotFound(path)), self.report_timing('exists', path):
                 self._s3_bucket().Object(path).load()
             return True
         except NotFound:
             return False
 
-    def delete(self, *args, **kw):
-        identifier, bucket = self.get_args_for_delete(*args, **kw)
-        path = self.get_path(identifier, bucket)
+    def delete(self, path):
+        check_safe_path(path)
         s3_bucket = self._s3_bucket()
+        deleted_bytes = 0
+        success = False
         with maybe_not_found():
             success = True
-            if identifier is None:
-                summaries = s3_bucket.objects.filter(Prefix=path + "/")
-                pages = ([{"Key": o.key} for o in page]
-                         for page in summaries.pages())
-                deleted_bytes = sum(o.size for page in summaries.pages()
-                                    for o in page)
-                deleted_count = 0
-                for objects in pages:
-                    resp = s3_bucket.delete_objects(Delete={"Objects": objects})
-                    deleted = set(d["Key"] for d in resp.get("Deleted", []))
-                    success = success and all(o["Key"] in deleted for o in objects)
-                    deleted_count += len(deleted)
-            else:
-                obj = s3_bucket.Object(path)
-                deleted_count = 1
-                # may raise a not found error -> return False
-                deleted_bytes = obj.content_length
-                obj.delete()
-            datadog_counter('commcare.blobs.deleted.count', value=deleted_count)
-            datadog_counter('commcare.blobs.deleted.bytes', value=deleted_bytes)
-            return success
-        return False
+            obj = s3_bucket.Object(path)
+            # may raise a not found error -> return False
+            deleted_bytes = obj.content_length
+            obj.delete()
+            success = True
+        self.metadb.delete(path, deleted_bytes)
+        return success
 
-    def bulk_delete(self, paths):
+    def bulk_delete(self, metas):
         success = True
-        for chunk in chunked(paths, 1000):
-            objects = [{"Key": path} for path in chunk]
+        for chunk in chunked(metas, 1000):
+            objects = [{"Key": meta.path} for meta in chunk]
             s3_bucket = self._s3_bucket()
-            deleted_bytes = 0
-            for path in chunk:
-                with maybe_not_found():
-                    deleted_bytes += s3_bucket.Object(path).content_length
             resp = s3_bucket.delete_objects(Delete={"Objects": objects})
             deleted = set(d["Key"] for d in resp.get("Deleted", []))
             success = success and all(o["Key"] in deleted for o in objects)
-            datadog_counter('commcare.blobs.deleted.count', value=len(deleted))
-            datadog_counter('commcare.blobs.deleted.bytes', value=deleted_bytes)
+            self.metadb.bulk_delete(chunk)
         return success
 
-    def copy_blob(self, content, info, bucket):
-        self._s3_bucket(create=True)
-        path = self.get_path(info.identifier, bucket)
-        with self.report_timing('copy_blobdb', info.identifier, bucket):
-            self._s3_bucket().upload_fileobj(content, path)
+    def copy_blob(self, content, meta):
+        with self.report_timing('copy_blobdb', meta.path):
+            self._s3_bucket(create=True).upload_fileobj(content, meta.path)
 
     def _s3_bucket(self, create=False):
         if create and not self._s3_bucket_exists:
@@ -162,11 +136,6 @@ class S3BlobDB(AbstractBlobDB):
                 self.db.create_bucket(Bucket=self.s3_bucket_name)
             self._s3_bucket_exists = True
         return self.db.Bucket(self.s3_bucket_name)
-
-    def get_path(self, identifier=None, bucket=DEFAULT_BUCKET):
-        if identifier is None:
-            return safepath(bucket)
-        return safejoin(bucket, identifier)
 
 
 class BlobStream(ClosingContextProxy):
@@ -181,30 +150,17 @@ class BlobStream(ClosingContextProxy):
         return self._blob_db()
 
 
-def safepath(path):
+def check_safe_path(path):
     if (path.startswith(("/", ".")) or
             "/../" in path or
             path.endswith("/..") or
             not SAFENAME.match(path)):
         raise BadName("unsafe path name: %r" % path)
-    return path
-
-
-def safejoin(root, subpath):
-    """Join root to subpath ensuring that the result is actually inside root
-    """
-    return safepath(root) + "/" + safepath(subpath)
 
 
 def is_not_found(err, not_found_codes=["NoSuchKey", "NoSuchBucket", "404"]):
     return (err.response["Error"]["Code"] in not_found_codes or
         err.response.get("Errors", {}).get("Error", {}).get("Code") in not_found_codes)
-
-
-def get_content_md5(content):
-    params = {"body": content, "headers": {}}
-    calculate_md5(params)
-    return params["headers"]["Content-MD5"]
 
 
 def get_file_size(fileobj):
