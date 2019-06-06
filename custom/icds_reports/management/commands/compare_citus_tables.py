d6b79e792d6c1cee878ecf5a484e52c969a7fac8
Simon Kelly <skelly@dimagi.com>
2019-06-06 12:37:32 +0200

    sort by pkey

--- custom/icds_reports/management/commands/compare_citus_tables.py
+++ custom/icds_reports/management/commands/compare_citus_tables.py
@@ -17,19 +17,36 @@ class Command(BaseCommand):
         parser.add_argument('citus_alias')
         parser.add_argument('monolith_alias')
 
-    def handle(self, citus_alias, monolith_alias, month, **options):
+    def handle(self, citus_alias, monolith_alias, **options):
         for table, _ in DISTRIBUTED_TABLES:
             citus_data = BytesIO()
             monolith_data = BytesIO()
             table_name = '{}'.format(table)
             with connections[citus_alias].cursor() as c:
-                c.copy_to(citus_data, table_name)
+                pk_cols = _get_table_pkey_cols(c, table_name)
+                q = 'select * from "{}" order by {}'.format(table_name, ', '.join(pk_cols))
+                c.copy_expert(q, citus_data)
                 citus_data.seek(0)
             with connections[monolith_alias].cursor() as c:
-                c.copy_to(monolith_data, table_name)
+                c.copy_expert(q, monolith_data)
                 monolith_data.seek(0)
             print('Diff for table {}'.format(table_name))
             diff = context_diff(citus_data.readlines(), monolith_data.readlines(), fromfile='citus', tofile='monolith')
             for d in diff:
                 print(d)
             print('--------------------')
+
+
+def _get_table_pkey_cols(cursor, table_name):
+    cursor.execute("""SELECT tbl.relname AS "table", array_agg(ind_column.attname) AS columns_of_pk
+    FROM pg_class tbl
+      INNER JOIN pg_namespace sch ON sch.oid = tbl.relnamespace
+      INNER JOIN pg_index ind ON ind.indrelid = tbl.oid
+      INNER JOIN pg_class ind_table ON ind_table.oid = ind.indexrelid
+      INNER JOIN pg_attribute ind_column ON ind_column.attrelid = ind_table.oid
+    WHERE sch.nspname <> 'pg_toast'
+      AND ind.indisprimary AND tbl.relname = %s
+    GROUP BY "table";
+    """, [table_name])
+
+    return cursor.fetchone()[1]
