a3f0ebafaf8f3cb84455ed4f1193b0ea913b2893
Simon Kelly <skelly@dimagi.com>
2019-06-06 17:23:06 +0200

    stuff

--- custom/icds_reports/management/commands/compare_citus_tables.py
+++ custom/icds_reports/management/commands/compare_citus_tables.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from datetime import datetime
 from difflib import context_diff
 from io import BytesIO
 
@@ -19,8 +20,11 @@ ignore_models = [
     'citusdashboarddiff',
     'citusdashboardtiming',
 
-    'aggawc',  # too big
-    'aggawcdaily',  # too big
+    # too big
+    'aggawc',
+    'aggawcdaily',
+    'aggawcdailyview',
+    'aggawcmonthly',
 
 ]
 
@@ -29,49 +33,132 @@ sort_fields = {
     'aggccsrecord': ['state_id', 'district_id', 'block_id', 'supervisor_id', 'awc_id'],
     'aggchildhealth': ['state_id', 'district_id', 'block_id', 'supervisor_id', 'awc_id'],
     'aggawcdaily': ['state_id', 'district_id', 'block_id', 'supervisor_id', 'awc_id'],
+
+    'dailyattendanceview': ['month', 'doc_id'],
+    'childhealthmonthlyview': ['month', 'case_id'],
+    'agglsmonthly': ['month', 'supervisor_id'],
+    'servicedeliverymonthly': ['month', 'awc_id'],
+    'awwincentivereportmonthly': ['month', 'awc_id'],
+    'aggccsrecordmonthly': ['month', 'awc_id'],
+    'ccsrecordmonthlyview': ['month', 'awc_id'],
+    'aggchildhealthmonthly': ['month', 'awc_id'],
+    'awclocationmonths': ['month', 'awc_id'],
+    'icds_disha_indicators': ['month', 'block_id'],
 }
+
+VIEWS = [
+    'dailyattendanceview',
+    'childhealthmonthlyview',
+    'agglsmonthly',
+    'servicedeliverymonthly',
+    'awwincentivereportmonthly',
+    'aggccsrecordmonthly',
+    'ccsrecordmonthlyview',
+    'aggchildhealthmonthly',
+    'awclocationmonths',
+    'dishaindicatorview',
+]
+
+DISTRIBUTED_MODELS = [
+    'aggregateccsrecorddeliveryforms',
+    'aggregatecomplementaryfeedingforms',
+    'aggregateccsrecordcomplementaryfeedingforms',
+    'aggregatechildhealththrforms',
+    'aggregategrowthmonitoringforms',
+    'aggregatechildhealthpostnatalcareforms',
+    'aggregateccsrecordpostnatalcareforms',
+    'aggregatebirthpreparednesforms',
+    'aggregateccsrecordthrforms',
+    'aggregatechildhealthdailyfeedingforms',
+    'childhealthmonthly',
+    'ccsrecordmonthly',
+    'dailyattendance',
+]
+
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('citus_alias')
         parser.add_argument('monolith_alias')
+        parser.add_argument('--group', choices=('all', 'distributed', 'views'), default='all')
+        parser.add_argument('--model', nargs='+')
 
     def handle(self, citus_alias, monolith_alias, **options):
+        models = options['model']
+        group = options['group']
+
         app = apps.get_app_config('icds_reports')
-        for name, model in app.models.items():
+
+        if models:
+            model_names = models
+        elif group == 'all':
+            model_names = list(app.models)
+        elif group == 'distributed':
+            model_names = DISTRIBUTED_MODELS
+        elif group == 'views':
+            model_names = VIEWS
+
+
+        for name in model_names:
+            model = app.models[name]
             if name in ignore_models:
                 print('Ignoring table {}'.format(name))
                 continue
 
-            citus_data = BytesIO()
-            monolith_data = BytesIO()
-
             table_name = model._meta.db_table
-            with connections[citus_alias].cursor() as c:
-                c.execute('select 1 from pg_views where viewname = %s', [table_name])
-                if c.fetchone():
-                    print("Skipping view: {}".format(table_name))
-                    continue
 
-                if name in sort_fields:
-                    sort_by = sort_fields[name]
-                else:
+            if name in sort_fields:
+                sort_by = sort_fields[name]
+            elif table_name in sort_fields:
+                sort_by = sort_fields[table_name]
+            else:
+                with connections[citus_alias].cursor() as c:
                     sort_by = _get_table_pkey_cols(c, table_name)
-                q = 'select * from "{}" order by {}'.format(table_name, ', '.join(sort_by))
+            if not sort_by:
+                print('Missing sort fields: {} ({})'.format(name, table_name))
+                continue
 
-                c.copy_expert(q, citus_data)
-                citus_data.seek(0)
+            if table_name in VIEWS or name in VIEWS:
+                month = datetime.utcnow().date().replace(day=1)
+                qname = 'Data for month: {}'.format(month.isoformat())
+                query = 'select * from "{}" where month = \'{}\' order by {}'.format(
+                    table_name, month.isoformat(), ', '.join(sort_by)
+                )
+            else:
+                qname = 'All data'
+                query = 'select * from "{}" order by {}'.format(table_name, ', '.join(sort_by))
 
-            with connections[monolith_alias].cursor() as c:
-                c.copy_expert(q, monolith_data)
-                monolith_data.seek(0)
+            _run_diff(table_name, qname, query, citus_alias, monolith_alias)
 
-            print('Diff for table {}'.format(table_name))
-            diff = context_diff(citus_data.readlines(), monolith_data.readlines(), fromfile='citus', tofile='monolith')
-            for d in diff:
-                print(d)
-            print('--------------------')
+
+def _run_diff(table_name, qname, query, citus_alias, monolith_alias):
+    print('Diff for table {} ({})'.format(table_name, qname))
+    citus_data = BytesIO()
+    monolith_data = BytesIO()
+
+    with connections[citus_alias].cursor() as c:
+        try:
+            c.copy_expert('COPY ({}) TO STDOUT WITH CSV HEADER'.format(query), citus_data)
+            citus_data.seek(0)
+        except Exception as e:
+            print("\tError querying citus")
+            print("\t\t{}".format(e))
+            return
+
+    with connections[monolith_alias].cursor() as c:
+        c.copy_expert(query, monolith_data)
+        monolith_data.seek(0)
+
+    monolith_lines = list(monolith_data.readlines())
+    citus_lines = list(citus_data.readlines())
+    if monolith_lines or citus_lines:
+        diff = context_diff(citus_lines, monolith_lines, fromfile='citus', tofile='monolith')
+        for d in diff:
+            print(d)
+    else:
+        print('\tNo data for table {}'.format(table_name))
+    print('--------------------')
 
 
 def _get_table_pkey_cols(cursor, table_name):
